cccc8d370335fde92b2335c819cb02a673b8a3e5
mre55 <misganawmihreteab>
2022-01-04 22:40:57 +0300

    Delete all the boilerplate from CRA

---
+++ src/components/Calculator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default Calculator;
