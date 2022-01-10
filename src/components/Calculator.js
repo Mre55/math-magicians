1a4bb5516fd0acdb1427cf3cf57a05c4f0ab6e9e
mre55 <misganawmihreteab>
2022-01-10 20:53:56 +0300

    Add css by using module and install react-route

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
+import styles from './Calculator.module.css';
 
 const Calculator = () => {
   const [stateObj, setStateObj] = useState({ total: '0' });
@@ -10,43 +11,48 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator-container">
-      <article className="article-class">
-        <div className="result-display">
-          { stateObj.total }
-          { stateObj.operation }
-          { stateObj.next }
-        </div>
-      </article>
-      <article className="article-class">
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">AC</button>
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">+/-</button>
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">%</button>
-        <button onClick={onClickHandler} type="button" className="last-one-fourth-class">÷</button>
-      </article>
-      <article className="article-class">
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">7</button>
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">8</button>
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">9</button>
-        <button onClick={onClickHandler} type="button" className="last-one-fourth-class">x</button>
-      </article>
-      <article className="article-class">
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">4</button>
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">5</button>
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">6</button>
-        <button onClick={onClickHandler} type="button" className="last-one-fourth-class">-</button>
-      </article>
-      <article className="article-class">
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">1</button>
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">2</button>
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">3</button>
-        <button onClick={onClickHandler} type="button" className="last-one-fourth-class">+</button>
-      </article>
-      <article className="article-class">
-        <button onClick={onClickHandler} type="button" className="first-one-fourth-class">0</button>
-        <button onClick={onClickHandler} type="button" className="one-fourth-class">.</button>
-        <button onClick={onClickHandler} type="button" className="last-one-fourth-class">=</button>
-      </article>
+    <div className={styles.calculator_container}>
+      <section className={styles.left_section}>
+        <h2>Let&apos;s do some math!</h2>
+      </section>
+      <section className={styles.right_section}>
+        <article className={styles.article_class}>
+          <div className={styles.result_display}>
+            { stateObj.total }
+            { stateObj.operation }
+            { stateObj.next }
+          </div>
+        </article>
+        <article className={styles.article_class}>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>AC</button>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>+/-</button>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>%</button>
+          <button onClick={onClickHandler} type="button" className={styles.last_one_fourth_class}>÷</button>
+        </article>
+        <article className={styles.article_class}>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>7</button>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>8</button>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>9</button>
+          <button onClick={onClickHandler} type="button" className={styles.last_one_fourth_class}>x</button>
+        </article>
+        <article className={styles.article_class}>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>4</button>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>5</button>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>6</button>
+          <button onClick={onClickHandler} type="button" className={styles.last_one_fourth_class}>-</button>
+        </article>
+        <article className={styles.article_class}>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>1</button>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>2</button>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>3</button>
+          <button onClick={onClickHandler} type="button" className={styles.last_one_fourth_class}>+</button>
+        </article>
+        <article className={styles.article_class}>
+          <button onClick={onClickHandler} type="button" className={styles.first_one_fourth_class}>0</button>
+          <button onClick={onClickHandler} type="button" className={styles.one_fourth_class}>.</button>
+          <button onClick={onClickHandler} type="button" className={styles.last_one_fourth_class}>=</button>
+        </article>
+      </section>
     </div>
   );
 };
